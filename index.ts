49784e89e80b0f55efa32db5ed38740659cc3c33
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-09-22 12:50:54 -0500

    Prevent double computes in same graph traversal

--- index.ts
+++ index.ts
@@ -1,43 +1,63 @@
 let id = -1
 function Id() { return ++id }
 
-let stackFrame = -1
-const stackFrames: Array<number> = []
+let stackGen = 0
+let stackFrame = 1
+const stackFrames = [-1]
+
+const SAME_GEN_PUT =
+  `Cannot update signal twice in same generation of stack frames`
 
 type SignalBuilder = {
   new<T>(x: T) : Signal<T>
 }
 
-const ents: Map<number,Signal<unknown>|Computed<unknown>> = new Map
-
-function evaluate(ids: Array<number>, num: number) {
-  for (let i = 0, id: number,  c: Computed<unknown>; i < num; i++) {
-    id = ids[i]
-    if (!ents.has(id)) throw new Error('Missing computation ${id}!')
-    c = ents.get(id) as Computed<unknown>
-    c.put()
-  }
-}
+const signalMap: Map<number,Signal<unknown>|Computed<unknown>> = new Map
+const dirtyMap: Map<number,boolean> = new Map
 
 class Signal<T> {
   private readonly id = Id()
   private deps: Array<number> = []
-  private depsNum = 0
-  constructor(private x: T) {}
+  private numDeps = 0
+  private genUpdated = -1
+  constructor(private x: T) {
+    signalMap.set(this.id, this)
+    dirtyMap.set(this.id, true)
+  }
 
   get() : T {
-    console.log(`Pulled ${this.id}`)
-    if (stackFrame !== -1) {
-      this.depsNum = this.deps.push(stackFrame)
+    const { id } = this
+    const { deps } = this
+    const { genUpdated } = this
+    const currentId = stackFrames[stackFrame]
+    if (genUpdated !== stackGen) {
+      this.genUpdated = stackGen
     }
+    if (stackFrame === 1) stackGen++
+    if (currentId !== -1 && !deps.includes(currentId)) {
+      this.numDeps = this.deps.push(currentId)
+    }
+    console.log(`Visited ${id}`)
     return this.x
   }
 
   put(x: T) : Signal<T> {
-    const deps = this.deps
-    this.x = x
-    for (let end = deps.length, i = 0; i < end; i++) {
+    const { id } = this
+    const { genUpdated } = this
+    const isValid =
+      stackFrame === 1 ||
+      genUpdated !== stackGen
+
+    if (!isValid) {
+      const msg = `${SAME_GEN_PUT}: id#${id} - ` +
+        `current frame id: ${stackFrames[stackFrame]} - ` +
+        `current generation: ${stackGen} - ` +
+        `last updated generation: ${genUpdated}`
+      throw new Error(msg)
     }
+
+    this.x = x
+    dirtyMap.set(id, true)
     return this
   }
 
@@ -45,21 +65,32 @@ class Signal<T> {
 }
 
 class Computed<T> {
-  private memo: unknown
+  private memo: T
   private readonly id = Id()
-  private dependencies: Array<number> = []
+  private deps: Array<number> = []
+  private numDeps = 0
+  private genVisited = -1
   constructor(private readonly fn: () => T) {}
   get() :T {
-    const currFrame = stackFrame
-    if (currFrame !== -1) this.dependencies.push(currFrame)
-    stackFrame = this.id
+    const { id } = this
+    const { deps } = this
+    const { genVisited } = this
+    const currentId = stackFrames[stackFrame]
+    if (stackFrame === 0) stackGen++
+    if (currentId !== -1 && !deps.includes(currentId)) {
+      this.numDeps = this.deps.push(currentId)
+    }
+    if (genVisited === stackGen) {
+      console.log(`Visited ${id} - No Compute`)
+      return this.memo
+    }
+    stackFrame = stackFrames.push(id)
     const value = this.memo = this.fn()
-    stackFrame = currFrame
-    console.log(`Pulled ${this.id}`)
+    this.genVisited = stackGen
+    stackFrames.pop()
+    stackFrame--
+    console.log(`Visited ${id} - Had Compute`)
     return value
-  }
-  put() {
-    this.memo = this.fn()
   }
 
   static of<T>(fn: () => T) : Computed<T> { return new Computed(fn) }
@@ -74,4 +105,13 @@ console.log(sum.get())
 s1.put(10)
 
 console.log(sum.get())
+
+const firstCharS = Signal.of("B")
+const firstChar = Computed.of(() => firstCharS.get())
+const firstName = Computed.of(() => `${firstChar.get()}enoit`)
+const lastName = Computed.of(() => `${firstChar.get()}arberousse`)
+const fullName = Computed.of(() => `${firstName.get()} ${lastName.get()}`)
+
+console.log(fullName.get())
+
 
